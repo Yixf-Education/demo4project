90bb2b4ddf91320e2b31d9b2139e29c3ad9c8417
Yixf-Research <yixianfu@tmu.edu.cn>
2018-03-21 09:51:46 +0800

    Run FastQC on raw reads

--- project_conserved_lncRNA/cmd.sh
+++ project_conserved_lncRNA/cmd.sh
@@ -2,8 +2,11 @@
 
 # Download reads from GEO/SRA database
 source activate NCBI
-nohup 00_bin/00.1_gsm2srr2fq.sh -d 01_reads -i 00_info/SEQC_brain.txt > 01_reads/download.log 2>&1 &
+nohup 00_bin/01.1_gsm2srr2fq.sh -d 01_reads -i 00_info/SEQC_brain.txt > 01_reads/download.log 2>&1 &
 source deactivate NCBI
 
 # Merge to get final FASTQ files
-nohup 00_bin/00.2_multi2one_srr2gsm.sh -d 01_reads  > 01_reads/merge.log 2>&1 &
+nohup 00_bin/01.2_multi2one_srr2gsm.sh -d 01_reads  > 01_reads/merge.log 2>&1 &
+
+# FastQC all reads
+bash 00_bin/01.3_fastqc.sh
