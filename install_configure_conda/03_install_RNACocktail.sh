e148873458e702fe90163dc20fafd532f294a152
Yixf-Research <yixianfu@tmu.edu.cn>
2018-03-21 09:13:57 +0800

    Download and merge SRR/GSM

---
+++ install_configure_conda/03_install_RNACocktail.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# https://bioinform.github.io/rnacocktail/
+
+conda create -n RNACocktail
+
+source activate RNACocktail
+
+# Python2
+conda install python=2
+
+# Dependencies
+conda install pybedtools pysam numpy scipy biopython openpyxl pandas xlrd
+
+# Tools
+conda install samtools hisat2 stringtie salmon oases velvet subread lordec star gatk picard htslib bowtie bowtie2 bwa sra-tools coreutils pigz blat ucsc-fatotwobit ucsc-liftover seqtk gmap
+
+# Packages
+conda install bioconductor-deseq2 bioconductor-tximport
+# Use R to install readr
+
+# Tools are not installed!
+# IDP: Long-read transcriptome reconstruction
+# IDP-fusion: Long-read fusion detection
+# GIREMI: RNA editing detection
+# FusionCatcher: RNA Fusion detection
+# gatb-core
+
+# RNACocktail
+pip install https://github.com/bioinform/RNACocktail/archive/v0.2.2.tar.gz
+
+source deactivate RNACocktail
